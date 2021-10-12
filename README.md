cd4aa1156fa8f894723de090ec568e7114ae118f
nohi <nohi244@gmail.com>
2021-10-13 08:26:44 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+vite + handlebars(partial html) + sass
---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import * as path from 'path';
+import handlebars from 'vite-plugin-handlebars';
+import { glob } from 'glob';
+
+const srcDir = 'src'
+
+// search html files
+const htmlGlob = glob.sync(`${srcDir}/**/*.html`, {
+    ignore: `${srcDir}/index.html`,
+}).map(key => [key.replace('src/', ''), path.resolve(__dirname, key)])
+
+rollupOptionsInput = {
+    main: path.resolve(__dirname, srcDir, 'index.html'),
+    ...Object.fromEntries(htmlGlob)
+}
+
+export default {
+    root: srcDir,
+    publicDir: 'public',
+    build: {
+        outDir: '../dist', // PATHはindex.htmlが基準
+        emptyOutDir: true,
+        rollupOptions: {
+            input: rollupOptionsInput,
+            // input: {
+            //     main: path.resolve(__dirname, srcDir, 'index.html'),
+            //     // privacy: path.resolve(__dirname, root, 'subdir/sub.html')
+            // }
+        }
+    },
+    plugins: [
+        handlebars({
+            reloadOnPartialChange: true,
+            partialDirectory: path.resolve(__dirname, srcDir, 'partials'),
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
+};
